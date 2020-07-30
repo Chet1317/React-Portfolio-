4fa585f6e723bbd626b8456f6a3953db296c95a9
Achilles <63617922+Chet1317@users.noreply.github.com>
2020-07-29 20:37:09 -0400

    created initial styling for navbar

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <nav className="navbar navbar-expand-lg navbar-light">
+  <a style={{color: "rgb(212,240,89)"}} class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li style={{marginLeft:"900px"}}  class="nav-item active">
+        <a style={{color: "rgb(212,240,89)"}} class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a style={{color: "rgb(212,240,89)"}} class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a style={{color: "rgb(212,240,89)"}} class="nav-link" href="#">Pricing</a>
+      </li>
+    </ul>
+  </div>
+</nav>
     </div>
   );
 }
