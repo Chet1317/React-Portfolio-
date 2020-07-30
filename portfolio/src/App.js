d5fa8a53bb36777ac9025e3d26807195bf0c1d0d
Achilles <63617922+Chet1317@users.noreply.github.com>
2020-07-30 19:27:51 -0400

    added final touch ups to styling for front page

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -7,20 +7,20 @@ function App() {
     <div className="App">
       
    <nav className="navbar navbar-expand-lg navbar-light">
-  <a style={{fontVariant:"small-caps", fontSize:"50px", color: "white"}} class="navbar-brand">Chet Martin</a>
+  <a style={{fontFamily:"Permanent Marker", fontVariant:"small-caps", fontSize:"50px", color: "Orange"}} class="navbar-brand">Chet Martin</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li style={{ marginLeft:"600px"}}  class="nav-item active">
-        <a style={{fontSize:"30px", color: "white"}} class="nav-link" href="#">About <span class="sr-only">(current)</span></a>
+        <a style={{fontFamily:"Saira Stencil One",fontSize:"30px", color: "teal"}} class="nav-link" href="#">About <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a style={{fontSize:"30px", color: "white"}} class="nav-link" href="#">Portfolio</a>
+        <a style={{fontFamily:"Saira Stencil One", fontSize:"30px", color: "teal"}} class="nav-link" href="#">Portfolio</a>
       </li>
       <li class="nav-item">
-        <a style={{fontSize:"30px", color: "white"}} class="nav-link" href="#">Contact</a>
+        <a style={{fontFamily:"Saira Stencil One",fontSize:"30px", color: "teal"}} class="nav-link" href="#">Contact</a>
       </li>
     </ul>
   </div>
@@ -28,9 +28,14 @@ function App() {
 
 <div class="jumbotron jumbotron-fluid">
   <div class="container">
-    <h1 class="display-4">Chet Martin</h1>
-    <p class="lead">Full Stack Developer</p>
+    <div class="row">
+    <div class="col-sm-3">
+    <h1 class="display-4">Full Stack Developer</h1>
+    <p class="lead">I am a Full Stack Web Developer in the Toronto area.</p>
+    <p class="lead">Check out the link to my Portfolio.</p>
   </div>
+  </div>
+</div>
 </div>
 </div>
     
