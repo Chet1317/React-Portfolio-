e468fc3c71f77d52180972be3ade27d276fd04f0
Achilles <63617922+Chet1317@users.noreply.github.com>
2020-07-30 00:06:34 -0400

    create the styling for the opening page, the background image, photo and navbar

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,29 +1,39 @@
 import React from 'react';
 import './App.css';
+import "./index.css"
 
 function App() {
   return (
     <div className="App">
+      
    <nav className="navbar navbar-expand-lg navbar-light">
-  <a style={{color: "rgb(212,240,89)"}} class="navbar-brand" href="#">Navbar</a>
+  <a style={{fontVariant:"small-caps", fontSize:"50px", color: "white"}} class="navbar-brand">Chet Martin</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
-      <li style={{marginLeft:"900px"}}  class="nav-item active">
-        <a style={{color: "rgb(212,240,89)"}} class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      <li style={{ marginLeft:"600px"}}  class="nav-item active">
+        <a style={{fontSize:"30px", color: "white"}} class="nav-link" href="#">About <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a style={{color: "rgb(212,240,89)"}} class="nav-link" href="#">Features</a>
+        <a style={{fontSize:"30px", color: "white"}} class="nav-link" href="#">Portfolio</a>
       </li>
       <li class="nav-item">
-        <a style={{color: "rgb(212,240,89)"}} class="nav-link" href="#">Pricing</a>
+        <a style={{fontSize:"30px", color: "white"}} class="nav-link" href="#">Contact</a>
       </li>
     </ul>
   </div>
 </nav>
-    </div>
+
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Chet Martin</h1>
+    <p class="lead">Full Stack Developer</p>
+  </div>
+</div>
+</div>
+    
   );
 }
 
