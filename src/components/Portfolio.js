d4b6db51eeca79bf0ceeeaa6d47b50b96967f9f8
Achilles <63617922+Chet1317@users.noreply.github.com>
2020-08-11 15:35:13 -0400

    added bookify card to portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -43,8 +43,8 @@ const Portfolio = () => {
                     <div >
                     <a style={{backgroundColor: "#000000b3", color: "lime", fontFamily: "permanent marker" }} href="https://still-woodland-95915.herokuapp.com/">"click"</a>
   <img src="https://user-images.githubusercontent.com/63617922/89857726-d54f9400-db6a-11ea-8356-c0aa83835f6f.png" class="card-img-top" alt="..."></img>
-  <div  style={{backgroundColor:"rgb(8, 8, 46, 0.70)"}}>
-    <p style={{color:"lightblue", fontFamily:"roboto", fontSize:"14px"}} class="card-text">EAT-DA-BURGER is a burger generating app was created with MySQL, Node, Express, Handlebars and a homemade ORM. Following the MVC design pattern; Node and MySQL were used to query and route the data and Handlebars were use to generate HTML.</p>
+  <div  style={{backgroundColor:"#0000008e"}}>
+    <p style={{ color:"lightblue", fontFamily:"roboto", fontSize:"14px"}} class="card-text">EAT-DA-BURGER is a burger generating app was created with MySQL, Node, Express, Handlebars and a homemade ORM. Following the MVC design pattern; Node and MySQL were used to query and route the data and Handlebars were use to generate HTML.</p>
   </div>
 </div>
 </div>
@@ -52,7 +52,7 @@ const Portfolio = () => {
                     <div >
                     <a style={{backgroundColor: "#000000b3", color: "#de9220", fontFamily: "permanent marker" }} href="https://afternoon-thicket-64187.herokuapp.com/">Note Taker</a>
   <img src="https://user-images.githubusercontent.com/63617922/89304640-aa7db100-d63b-11ea-8439-af3729246a07.png" class="card-img-top" alt="..."></img>
-  <div  style={{backgroundColor:"rgb(8, 8, 46, 0.70)"}}>
+  <div  style={{backgroundColor:"#0000009b"}}>
     <p style={{color:"lightblue", fontFamily:"roboto", fontSize:"14px"}} class="card-text">This application can be used to write, save, and delete notes. It uses an Express backend that saves and retrieves note data from a JSON file. Keep track of your workouts, grocery lists or anything you want with this application.</p>
   </div>
 </div>
@@ -61,7 +61,7 @@ const Portfolio = () => {
                     <div >
                     <a style={{backgroundColor: "#000000b3", color: "#de9220", fontFamily: "permanent marker" }} href="https://chet1317.github.io/Server-Side-APIs-Weather-Dashboard/">Weather App</a>
   <img src="https://user-images.githubusercontent.com/63617922/89857477-3460d900-db6a-11ea-80d1-5099a5ecedb1.png" class="card-img-top" alt="..."></img>
-  <div  style={{backgroundColor:"rgb(8, 8, 46, 0.70)"}}>
+  <div  style={{backgroundColor:"#0000008e"}}>
     <p style={{color:"lightblue", fontFamily:"roboto", fontSize:"14px"}} class="card-text">Using OpenWeather.com API's, this application retrieves a five day weather forecast for any city you wish to search. It also inculdes the humidity perecentage, UVI and wind speed. It's the perefect app if you want to search the weather for an upcoming trip.</p>
   </div>
 </div>
@@ -70,7 +70,7 @@ const Portfolio = () => {
                     <div >
                     <a style={{backgroundColor: "#000000b3", color: "#de9220", fontFamily: "permanent marker" }} href="https://lit-badlands-54539.herokuapp.com/">Me My Chef and I</a>
   <img src="https://user-images.githubusercontent.com/63617922/89603462-2ee85380-d837-11ea-885f-e892dd16708d.png" class="card-img-top" alt="..."></img>
-  <div  style={{backgroundColor:"rgb(8, 8, 46, 0.70)"}}>
+  <div  style={{backgroundColor:"#0000008e"}}>
     <p style={{color:"lightblue", fontFamily:"roboto", fontSize:"14px"}} class="card-text">"Me MyChef & I" is web application uses API'S from spoonacular.com. Simply click on the "Check Your Fridge" icon and you can view, add or remove food from your fridge. Update your fridge and click on the "Find Recipe" icon. Recipes will appear based on the updated fridge.</p>
   </div>
 </div>
@@ -79,7 +79,7 @@ const Portfolio = () => {
                     <div >
                     <a style={{backgroundColor: "#000000b3", color: "#de9220", fontFamily: "permanent marker" }}href="https://wesycool.github.io/Project1-Edmund/Project1-Shayanne/">nutShell</a>
   <img src="https://user-images.githubusercontent.com/63617922/89603847-f6954500-d837-11ea-87d4-ddd41adf7ae4.png" class="card-img-top" alt="..."></img>
-  <div  style={{backgroundColor:"rgb(8, 8, 46, 0.70)"}}>
+  <div  style={{backgroundColor:"#0000008e"}}>
     <p style={{color:"lightblue", fontFamily:"roboto", fontSize:"14px"}} class="card-text">A mobile first web application that aims to consolidate the digital organizational tools we employ on a daily basis into one space. This app removes need for multiple applications and the flood of irrelevant information people are often bombarded with.</p>
   </div>
 </div>
@@ -88,19 +88,30 @@ const Portfolio = () => {
                     <div >
                     <a style={{backgroundColor: "#000000b3", color: "#de9220", fontFamily: "permanent marker" }} href="https://chet1317.github.io/Web-APIs-Code-Quiz/">Code Quiz</a>
   <img src="https://user-images.githubusercontent.com/63617922/89604438-46c0d700-d839-11ea-86d4-cbb73ce87e49.png" class="card-img-top" alt="..."></img>
-  <div  style={{backgroundColor:"rgb(8, 8, 46, 0.70)"}}>
+  <div  style={{backgroundColor:"#0000008e"}}>
     <p style={{color:"lightblue", fontFamily:"roboto", fontSize:"14px"}} class="card-text">Code quiz is a fun little app that quizes the user JavaScript. The timer is set at 200 seconds, when the start button is pressed, the user is presented with a question. If the user answers incorrectly, time is stripped away from the clock. A final score is presented at the end. </p>
+  </div>
+</div>
+</div>
+<div class="col-md-4">
+<div >
+                    <a style={{backgroundColor: "#000000b3", color: "#de9220", fontFamily: "permanent marker" }} href="https://chet1317.github.io/Web-APIs-Code-Quiz/">Bookify</a>
+  <img src="https://user-images.githubusercontent.com/63617922/89940733-fa361c80-dbe7-11ea-8df3-78be989b8029.png" class="card-img-top" alt="..."></img>
+  <div  style={{backgroundColor:"#0000008e"}}>
+    <p style={{color:"lightblue", fontFamily:"roboto", fontSize:"14px"}} class="card-text">Bookify is a convenient web application designed to allow users to research and discover the ideal venue for whatever they are looking for. MERN Technologies were used to create this app, which includes; MongoDB, Express, React and Node. Other technologies were used as well. </p>
   </div>
 </div>
 </div>
 
                     </div>
                 </div>
-            </div>
+           
             <footer>
                 <p style={{ textAlign: "center", fontFamily: "satisfy" }}>(c) Chet Martin</p>
             </footer>
         </div>
+        </div>
+        
 
 
 
